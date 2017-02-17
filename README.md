08ae4b72233d370c59a41e2c0ac130d873a498fd
Frazer Jamieson <frazer@notebook.localdomain>
2017-02-17 16:59:09 +0000

    first commit - all files

---
+++ README.md
@@ -0,0 +1,2 @@
+# angularjs2-skeleton
+# AJS2-template
---
+++ app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component ({
+    selector: 'my-app',
+    template: '<h1>Skeleton Template</h1>'
+})
+
+export class AppComponent { }
